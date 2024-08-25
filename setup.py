b4bd8ea94bfefe02b247434b6f81323f6fbbea9c
pabitra biswas <p.pabitrabiswas02@gmail.com>
2024-08-25 12:26:54 +0530

    code updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-_version_ = "0.0.12"
+_version_ = "0.0.13"
 REPO_NAME = "mongodbconnector"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "Pabitra-Biswas"
